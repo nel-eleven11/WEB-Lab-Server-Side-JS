56e2dac5ac385035058739d5fb227ae81914f4ac
Nelson <gar22434@uvg.edu.gt>
2024-05-31 13:14:07 -0600

    Feat: Table posts done

--- schema.sql
+++ schema.sql
@@ -1,3 +1,13 @@
 CREATE USER IF NOT EXISTS 'nelson'@'%' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON blog_nelson.* TO 'nelson'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    posicion VARCHAR(100) NOT NULL,
+    posicion2 VARCHAR(100),
+    numero INT NOT NULL,
+    equipo VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL
+);
