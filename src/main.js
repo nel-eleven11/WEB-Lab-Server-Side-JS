0b0975544072f06889dac2e1dc0b5f6f120e42b1
Nelson <gar22434@uvg.edu.gt>
2024-05-31 13:06:11 -0600

    Feat: Get  endpoint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import conn from './conn.js';
+import { getAllPosts } from './db.js'
 
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from server')
@@ -9,4 +13,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server listening at http://127.0.0.1:${port}`)
+})
+
+app.get('/posts', async (req, res) => {
+    const posts = await getAllPosts()
+    res.json(posts)
 })
